cf947e08e447b6eca6822556ee5a6f685c744429
itcast <hello@itcast.cn>
2025-02-25 23:27:12 +0800

    'test'

--- lth-rpc/src/main/java/com/mszlu/rpc/netty/codec/MsRpcDecoder.java
+++ lth-rpc/src/main/java/com/mszlu/rpc/netty/codec/MsRpcDecoder.java
@@ -1,4 +1,9 @@
 package com.mszlu.rpc.netty.codec;
 
-public class MsRpcDecoder {
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+
+public class MsRpcDecoder extends LengthFieldBasedFrameDecoder {
+    public MsRpcDecoder(int maxFrameLength, int lengthFieldOffset, int lengthFieldLength) {
+        super(maxFrameLength, lengthFieldOffset, lengthFieldLength);
+    }
 }
